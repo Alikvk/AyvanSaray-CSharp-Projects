8d978625b65f6845ce1a965bb7338a57d706cebb
ALI KAVAK <ali.kavak1905@gmail.com>
2018-12-10 20:49:21 +0300

    10.12.2018

---
+++ HOMEWORK/Console Application/EnumTypeExample/EnumTypeExample/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumTypeExample
+{
+    class Program
+    {
+        //enum Seasons
+        //{
+        //    Winter,
+        //    Summer,
+        //    Autumn,
+        //    Sprimg
+
+        //}
+        // İndexi istediğimiz sayıdan başlatabiliriz.
+        enum Seasons
+        {
+            Winter=12,
+            Summer,
+            Autumn,
+            Sprimg
+
+        }
+        static void Main(string[] args)
+        {
+            // Seasons autumnS = Seasons.Autumn;
+            string[] mevsimler = new string[4];
+            mevsimler = Enum.GetNames(typeof(Seasons));// Elemanları bir diziye aktarıyoruz
+            foreach (var s in mevsimler)
+            {
+                Console.WriteLine(s);
+            }
+            Console.WriteLine((int)Seasons.Winter);
+            Console.WriteLine((int)Seasons.Sprimg);
+            Console.ReadKey();
+        }
+    }
+}
