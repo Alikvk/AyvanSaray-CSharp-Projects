f96491f3b90a219923a46fd3de43d07419e424f9
Ali Kavak <ali.kavak1905@gmail.com>
2019-01-14 13:38:24 +0300

    14.01.2019

---
+++ DERS KODLARI/Hafta 8/Hafta8Gun1/DovizKurlari/Doviz.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DovizKurlari
+{
+    class Doviz
+    {
+        public int Birim { get; set; }
+        public string DovizAdi { get; set; }
+        public decimal AlisFiyati { get; set; }
+        public decimal SatiFiyati { get; set; }
+
+        public override string ToString()
+        {// toString Methodu object içinden olduğunda heryerden ulaşılmaktadır.
+            // Böylelikle ToString methodunu override edebilmekteyiz
+            return DovizAdi + " " + Birim;
+        }
+    }
+}
