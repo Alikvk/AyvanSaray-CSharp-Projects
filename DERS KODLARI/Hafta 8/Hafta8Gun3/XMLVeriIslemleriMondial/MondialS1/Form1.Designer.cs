829e7aa67f05e938972bd243b3f4c859019e0810
Ali KAVAK <ali.kavak1905@gmail.com>
2019-01-16 12:04:40 +0300

    16.01.2019

---
+++ DERS KODLARI/Hafta 8/Hafta8Gun3/XMLVeriIslemleriMondial/MondialS1/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace MondialS1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstListele = new System.Windows.Forms.ListView();
+            this.btnListele = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // lstListele
+            // 
+            this.lstListele.Location = new System.Drawing.Point(28, 12);
+            this.lstListele.Name = "lstListele";
+            this.lstListele.Size = new System.Drawing.Size(276, 315);
+            this.lstListele.TabIndex = 0;
+            this.lstListele.UseCompatibleStateImageBehavior = false;
+            // 
+            // btnListele
+            // 
+            this.btnListele.Location = new System.Drawing.Point(325, 65);
+            this.btnListele.Name = "btnListele";
+            this.btnListele.Size = new System.Drawing.Size(139, 53);
+            this.btnListele.TabIndex = 1;
+            this.btnListele.Text = "Listele";
+            this.btnListele.UseVisualStyleBackColor = true;
+            this.btnListele.Click += new System.EventHandler(this.btnListele_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(470, 13);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(318, 331);
+            this.webBrowser1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.btnListele);
+            this.Controls.Add(this.lstListele);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lstListele;
+        private System.Windows.Forms.Button btnListele;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+    }
+}
+
---
+++ DERS KODLARI/Hafta 8/Hafta8Gun3/XMLVeriIslemleriMondial/MondialS1/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace MondialS1
+{
+    public partial class Form1 : Form
+    {
+        string dosyaYolu = Application.StartupPath +"\\mondial.xml";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void btnListele_Click(object sender, EventArgs e)
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(dosyaYolu);
+            XmlNode secilenNode = xmlDoc.ChildNodes[2];
+            foreach (XmlNode item in secilenNode)
+            {
+                if (item.Name=="country")
+                {
+                    lstListele.Items.Add(item.Attributes["name"].Value);
+                }
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            webBrowser1.Url = new Uri(dosyaYolu);
+        }
+    }
+}
