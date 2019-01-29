1513ce438f2f14f3eb2578b550815288e5c08b4f
Ali Kavak <ali.kavak1905@gmail.com>
2019-01-29 11:16:04 +0300

    Linq Projesi Eklendi

---
+++ DERS KODLARI/Hafta 10/Hafta10Gun1/LINQOrnek/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static LinqOrnekleri_VeriKaynagi.Class1; // Aşağıdali ogrencilergetir fonksiyonunu çağırırken önüne class1 şeklinde değer yazmamak için static yazıp class ismini kullanıyoruz.
+
+namespace LINQOrnek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var studentList = OgrencileriGetir();
+
+            var genderList = studentList.GroupBy(student => student.Cinsiyet);
+
+            var clubList = studentList.GroupBy(student => student.Kulup);
+            //key değeri içinde {student.Cinsiyet, student.Kulup} değerler vardır.
+            var genderAndClubList = studentList.GroupBy(
+                student => new { student.Cinsiyet, student.Kulup }, 
+                //anonymus typr aşağıdakinin adlandırıldığı tür
+                (key, studentListGroup) =>
+                 {
+                     return new
+                     {
+                         Count = studentListGroup.Count(),
+                         Student = studentListGroup
+                                  .Select(singleS => singleS.Adi + " " + singleS.Soyadi)
+                                  .OrderBy(x => x)
+                     };
+                 }
+
+                );
+
+        }
+    }
+}
