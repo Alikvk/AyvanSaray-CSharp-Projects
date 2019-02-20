0550e63b984376b64e86b228e836adad4e3476e8
Ali KAVAK <ali.kavak1905@gmail.com>
2019-02-20 13:59:28 +0300

    20.02.2019
    Circluid Template Nortwind Database MVC Project

---
+++ DERS KODLARI/Hafta 13/Hafta13Gun3/MVC_Template/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using MVC_Template.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Template.Controllers
+{
+    public class ProductController : Controller
+    {
+        NorthwindContext _model = new NorthwindContext();
+
+        // GET: Product
+        public ActionResult Index()
+        {
+            List<Product> product = _model.Products.ToList();
+            return View(product);
+        }
+    }
+}
